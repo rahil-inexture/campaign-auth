9db8575047f4b9720e94a56cd3d2e7a1b9df2371
rahil-inexture <rahil@inexture.com>
2023-05-26 11:00:49 +0530

    first commit code

---
+++ src/main/java/com/campaign/CampaignAuthApplication.java
@@ -0,0 +1,13 @@
+package com.campaign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaignAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampaignAuthApplication.class, args);
+	}
+
+}
